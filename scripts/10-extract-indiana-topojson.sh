4dbadc04bd9816032ba38f629abdca16d78e3900
Bruce Herr II <bherr@indiana.edu>
2019-12-03 17:01:38 -0500

    Added script to create a limited topojson of indiana counties Added a basic vega-lite visualization of indiana Added templates for putting pure vega charts in the site (for now)

---
+++ scripts/10-extract-indiana-topojson.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source constants.sh
+set -ev
+
+if [ "$(which topo2geo)" = "" ]; then
+  npm install --prefix=~/.local -g topojson ndjson-cli
+fi
+
+if [ ! -e $OUT/counties-10m.json ]; then
+  SRC_TOPOJSON=https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json
+  wget -O $OUT/counties-10m.json $SRC_TOPOJSON
+fi
+
+topo2geo -n counties=- < $OUT/counties-10m.json \
+  | ndjson-filter '/^18/.test(d.id)' \
+  | geo2topo -n counties=- \
+  > docs/geomap/indiana.topojson
