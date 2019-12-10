b457c87ac09c772f8b6bc5963f4ff38fe72f913b
Bruce Herr II <bherr@indiana.edu>
2019-12-10 15:27:02 -0500

    Add row-wise data generation for geomap (currently unused)

--- scripts/10-extract-geomap-data.sh
+++ scripts/10-extract-geomap-data.sh
@@ -11,3 +11,10 @@ sqlite3 $GEOMAP_DB > $OUT/site-data/geomap-data.csv << EOF
 SELECT * FROM all_aggregates ORDER BY period;
 .quit
 EOF
+
+sqlite3 $GEOMAP_DB > $OUT/site-data/geomap-data-row-based.csv << EOF
+.header on
+.mode csv
+SELECT * FROM all_aggregates_row_based ORDER BY data_variable, period, tract_5;
+.quit
+EOF
