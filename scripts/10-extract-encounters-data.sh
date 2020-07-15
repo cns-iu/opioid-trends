6a58a2c039e94ea89d4bd39a8ff12b23fa6daaa4
Adam Phillips <adjp2013@gmail.com>
2020-07-15 16:46:08 -0400

    encounters data extraction and visualization work

--- scripts/10-extract-encounters-data.sh
+++ scripts/10-extract-encounters-data.sh
@@ -6,9 +6,16 @@ set -ev
 rm -f $ENCOUNTERS_DB
 sqlite3 $ENCOUNTERS_DB < src/create-encounters-db.sql
 
-sqlite3 $ENCOUNTERS_DB > $OUT/site-data/encounters-data.csv << EOF
+sqlite3 $ENCOUNTERS_DB > $OUT/site-data/encounters-category-data.csv << EOF
 .header on
 .mode csv
 SELECT * FROM CATEGORY_COUNTS;
+.quit
+EOF
+
+sqlite3 $ENCOUNTERS_DB > $OUT/site-data/encounters-insurance-data.csv << EOF
+.header on
+.mode csv
+SELECT * FROM INSURANCE_COUNTS;
 .quit
 EOF
