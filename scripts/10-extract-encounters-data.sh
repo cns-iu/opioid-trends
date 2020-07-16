2ed177ddcddefc1d8aa14b9b2a8785a2e3ada127
Adam Phillips <adjp2013@gmail.com>
2020-07-16 15:44:40 -0400

    encounters work, cohorts, concatenating.

--- scripts/10-extract-encounters-data.sh
+++ scripts/10-extract-encounters-data.sh
@@ -4,6 +4,7 @@ set -ev
 
 # Data extraction goes here!
 rm -f $ENCOUNTERS_DB
+sqlite3_ext $ENCOUNTERS_DB < src/chronic.sql
 sqlite3 $ENCOUNTERS_DB < src/create-encounters-db.sql
 
 sqlite3 $ENCOUNTERS_DB > $OUT/site-data/encounters-category-data.csv << EOF
@@ -16,6 +17,6 @@ EOF
 sqlite3 $ENCOUNTERS_DB > $OUT/site-data/encounters-insurance-data.csv << EOF
 .header on
 .mode csv
-SELECT * FROM INSURANCE_COUNTS;
+SELECT * FROM INSURANCE_COUNTS_AGG;
 .quit
 EOF
