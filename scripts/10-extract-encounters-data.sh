129ac6aac45c56104994a288ec2187038f1a6b49
Adam Phillips <adjp2013@gmail.com>
2020-07-17 15:02:34 -0400

    encounters data close to ready, visualization work

--- scripts/10-extract-encounters-data.sh
+++ scripts/10-extract-encounters-data.sh
@@ -10,13 +10,13 @@ sqlite3 $ENCOUNTERS_DB < src/create-encounters-db.sql
 sqlite3 $ENCOUNTERS_DB > $OUT/site-data/encounters-category-data.csv << EOF
 .header on
 .mode csv
-SELECT * FROM CATEGORY_COUNTS;
+SELECT * FROM CATEGORY_AGG;
 .quit
 EOF
 
-sqlite3 $ENCOUNTERS_DB > $OUT/site-data/encounters-insurance-data.csv << EOF
-.header on
-.mode csv
-SELECT * FROM INSURANCE_COUNTS_AGG;
-.quit
-EOF
+# sqlite3 $ENCOUNTERS_DB > $OUT/site-data/encounters-insurance-data.csv << EOF
+# .header on
+# .mode csv
+# SELECT * FROM INSURANCE_COUNTS_AGG;
+# .quit
+# EOF
