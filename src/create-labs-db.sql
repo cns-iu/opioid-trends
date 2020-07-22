1d4e3646c5ed929e9f9e03aecda75964d285e012
Adam Phillips <adjp2013@gmail.com>
2020-07-22 15:08:57 -0400

    rewrote sql queries to pull the correct figures for graphing

--- src/create-labs-db.sql
+++ src/create-labs-db.sql
@@ -5,71 +5,77 @@ ATTACH DATABASE 'raw-data/original/box-health/R2767_Data.db' AS src;
 
 
 -- (O)pioid related events --
-DROP TABLE IF EXISTS "LABS_COUNT";
-CREATE TABLE "LABS_COUNT" AS
-    SELECT
-        strftime('%Y', TEST_DATE) || CASE 
-            WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 1 AND 3 THEN '-01-01'
-            WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 4 and 6 THEN '-04-01'
-            WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 7 and 9 THEN '-07-01'
-            ELSE '-10-01'
-            END AS "PERIOD", -- Quarterly Period
-        COUNT(*) AS TOTAL_COUNT
-    FROM src.LABS
-    GROUP BY PERIOD;
 
-DROP TABLE IF EXISTS "PRE_LABS_ALL";
-CREATE TABLE "PRE_LABS_ALL" AS
+DROP TABLE IF EXISTS "ALL_LABS_COUNT";
+CREATE TABLE "ALL_LABS_COUNT" AS
     SELECT
-        strftime('%Y', TEST_DATE) || CASE 
-            WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 1 AND 3 THEN '-01-01'
-            WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 4 and 6 THEN '-04-01'
-            WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 7 and 9 THEN '-07-01'
-            ELSE '-10-01'
-            END AS "PERIOD", -- Quarterly Period
-        LAB_CODE_TYPE
-    FROM src.LABS;
+        COUNT(DISTINCT STUDY_ID) AS TOTAL_COUNT
+    FROM src.DEMOGRAPHICS;
+
+DROP TABLE IF EXISTS "CHRONIC_LABS_COUNT";
+CREATE TABLE "CHRONIC_LABS_COUNT" AS
+    SELECT
+        COUNT(DISTINCT STUDY_ID) AS TOTAL_COUNT
+    FROM src.DEMOGRAPHICS
+    WHERE THREE_FILL_FLAG IS 1;
+
+DROP TABLE IF EXISTS "OTHER_LABS_COUNT";
+CREATE TABLE "OTHER_LABS_COUNT" AS
+    SELECT
+        COUNT(DISTINCT STUDY_ID) AS TOTAL_COUNT
+    FROM src.DEMOGRAPHICS
+    WHERE ONE_PER_YEAR_FLAG IS 1;
+
+
+DROP TABLE IF EXISTS "LABS_ADD_FLAGS";
+CREATE TABLE "LABS_ADD_FLAGS" AS
+    SELECT
+        STUDY_ID,
+        TEST_DATE,
+        LAB_CODE_TYPE,
+        THREE_FILL_FLAG,
+        ONE_PER_YEAR_FLAG
+    FROM src.LABS
+    LEFT JOIN src.DEMOGRAPHICS
+    USING(STUDY_ID);
 
 DROP TABLE IF EXISTS "LABS_ALL";
 CREATE TABLE "LABS_ALL" AS
     SELECT
-        PERIOD,
-        LAB_CODE_TYPE,
-        COUNT(LAB_CODE_TYPE) as LAB_COUNT,
-        TOTAL_COUNT
-    FROM PRE_LABS_ALL
-    JOIN LABS_COUNT USING(PERIOD)
-    GROUP BY PERIOD, LAB_CODE_TYPE
-    ORDER BY PERIOD, LAB_CODE_TYPE;
-
-DROP TABLE IF EXISTS "PRE_LABS_CHRONIC";
-CREATE TABLE "PRE_LABS_CHRONIC" AS
-    SELECT
         strftime('%Y', TEST_DATE) || CASE 
             WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 1 AND 3 THEN '-01-01'
             WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 4 and 6 THEN '-04-01'
             WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 7 and 9 THEN '-07-01'
             ELSE '-10-01'
             END AS "PERIOD", -- Quarterly Period
-        LAB_CODE_TYPE
-    FROM src.LABS
-    JOIN CHRONIC USING(STUDY_ID)
-    WHERE IS_CHRONIC = 1;
+        LAB_CODE_TYPE,
+        COUNT(LAB_CODE_TYPE) AS LAB_TYPE_COUNT,
+        TOTAL_COUNT
+    FROM LABS_ADD_FLAGS
+    JOIN ALL_LABS_COUNT
+    GROUP BY PERIOD, LAB_CODE_TYPE
+    ORDER BY PERIOD, LAB_CODE_TYPE;
 
 DROP TABLE IF EXISTS "LABS_CHRONIC";
 CREATE TABLE "LABS_CHRONIC" AS
     SELECT
-        PERIOD,
+        strftime('%Y', TEST_DATE) || CASE 
+            WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 1 AND 3 THEN '-01-01'
+            WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 4 and 6 THEN '-04-01'
+            WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 7 and 9 THEN '-07-01'
+            ELSE '-10-01'
+            END AS "PERIOD", -- Quarterly Period
         LAB_CODE_TYPE,
-        COUNT(LAB_CODE_TYPE) as LAB_COUNT,
+        COUNT(LAB_CODE_TYPE) AS LAB_TYPE_COUNT,
         TOTAL_COUNT
-    FROM PRE_LABS_CHRONIC
-    JOIN LABS_COUNT USING(PERIOD)
+    FROM LABS_ADD_FLAGS
+    JOIN CHRONIC_LABS_COUNT
+    WHERE THREE_FILL_FLAG IS 1
     GROUP BY PERIOD, LAB_CODE_TYPE
     ORDER BY PERIOD, LAB_CODE_TYPE;
 
-DROP TABLE IF EXISTS "PRE_LABS_OTHER";
-CREATE TABLE "PRE_LABS_OTHER" AS
+DROP TABLE IF EXISTS "LABS_OTHER";
+CREATE TABLE "LABS_OTHER" AS
     SELECT
         strftime('%Y', TEST_DATE) || CASE 
             WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 1 AND 3 THEN '-01-01'
@@ -77,20 +83,12 @@ CREATE TABLE "PRE_LABS_OTHER" AS
             WHEN cast(strftime('%m', TEST_DATE) as integer) BETWEEN 7 and 9 THEN '-07-01'
             ELSE '-10-01'
             END AS "PERIOD", -- Quarterly Period
-        LAB_CODE_TYPE
-    FROM src.LABS
-    JOIN CHRONIC USING(STUDY_ID)
-    WHERE IS_CHRONIC = 1;
-
-DROP TABLE IF EXISTS "LABS_OTHER";
-CREATE TABLE "LABS_OTHER" AS
-    SELECT
-        PERIOD,
         LAB_CODE_TYPE,
-        COUNT(LAB_CODE_TYPE) as LAB_COUNT,
+        COUNT(LAB_CODE_TYPE) AS LAB_TYPE_COUNT,
         TOTAL_COUNT
-    FROM PRE_LABS_OTHER
-    JOIN LABS_COUNT USING(PERIOD)
+    FROM LABS_ADD_FLAGS
+    JOIN OTHER_LABS_COUNT
+    WHERE ONE_PER_YEAR_FLAG IS 1
     GROUP BY PERIOD, LAB_CODE_TYPE
     ORDER BY PERIOD, LAB_CODE_TYPE;
 
@@ -100,7 +98,7 @@ CREATE TABLE "LABS_AGG" AS
         SELECT
             PERIOD,
             LAB_CODE_TYPE,
-            LAB_COUNT,
+            LAB_TYPE_COUNT,
             TOTAL_COUNT,
             "ALL" AS "COHORT"
         FROM LABS_ALL
@@ -108,7 +106,7 @@ CREATE TABLE "LABS_AGG" AS
         SELECT
             PERIOD,
             LAB_CODE_TYPE,
-            LAB_COUNT,
+            LAB_TYPE_COUNT,
             TOTAL_COUNT,
             "CHRONIC" AS "COHORT"
         FROM LABS_CHRONIC
@@ -116,7 +114,7 @@ CREATE TABLE "LABS_AGG" AS
         SELECT
             PERIOD,
             LAB_CODE_TYPE,
-            LAB_COUNT,
+            LAB_TYPE_COUNT,
             TOTAL_COUNT,
             "OTHER" AS "COHORT"
         FROM LABS_OTHER
