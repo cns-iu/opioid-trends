0224af1a72de0cca54f2027eaecbfc4f6af62380
Adam Phillips <adjp2013@gmail.com>
2020-07-24 16:30:02 -0400

    encounters sql changed to row-based;  visualization in progress

--- scripts/10-extract-encounters-data.sh
+++ scripts/10-extract-encounters-data.sh
@@ -4,15 +4,16 @@ set -ev
 
 # Data extraction goes here!
 rm -f $ENCOUNTERS_DB
-sqlite3_ext $ENCOUNTERS_DB < src/chronic.sql
+# REMOVE CHRONIC TABLE AND UPDATE SQL
+# sqlite3_ext $ENCOUNTERS_DB < src/chronic.sql
 sqlite3 $ENCOUNTERS_DB < src/create-encounters-db.sql
 
-sqlite3 $ENCOUNTERS_DB > $OUT/site-data/encounters-category-data.csv << EOF
-.header on
-.mode csv
-SELECT * FROM CATEGORY_AGG;
-.quit
-EOF
+# sqlite3 $ENCOUNTERS_DB > $OUT/site-data/encounters-category-data.csv << EOF
+# .header on
+# .mode csv
+# SELECT * FROM CATEGORY_AGG;
+# .quit
+# EOF
 
 # sqlite3 $ENCOUNTERS_DB > $OUT/site-data/encounters-insurance-data.csv << EOF
 # .header on
